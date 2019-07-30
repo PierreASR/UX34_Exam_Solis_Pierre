0ce4973629cf464f81e23c37a063397f53b25881
Pierre Solis <al1609115@upy.edu.mx>
2019-07-29 21:26:05 -0500

    controller added

--- controllers/UX34_Exam_Solis_Pierre/UX34_Exam_Solis_Pierre.c
+++ controllers/UX34_Exam_Solis_Pierre/UX34_Exam_Solis_Pierre.c
@@ -14,7 +14,7 @@
 #include <webots/motor.h>
 #include <webots/distance_sensor.h>
 
-
+#include <stdio.h>
 /*
  * You may want to add macros here.
  */
@@ -36,7 +36,14 @@ int main(int argc, char **argv) {
    *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
    *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
    */
-
+   WbDeviceTag left = wb_robot_get_device ("motor_left");
+   WbDeviceTag right = wb_robot_get_device ("motor_right");
+   
+   WbDeviceTag posision1 = wb_robot_get_device ("encoder1");
+   WbDeviceTag position2 = wb_robot_get_device ("encoder2");
+   
+   WbDeviceTag dis_left = wb_robot_get_device ("dis_senosr1");
+   WbDeviceTag dis_rigth = wb_robot_get_device ("dis_sensor2");
   /* main loop
    * Perform simulation steps of TIME_STEP milliseconds
    * and leave the loop when the simulation is over
